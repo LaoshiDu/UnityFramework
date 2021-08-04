f2b843c06ccc62cfcc43850f0a0e69c098b6df8e
JGM <1142579287@qq.com>
2021-08-04 19:16:11 +0800

    excel转json和C#代码

--- UnityFramework/Assets/Frame/Scripts/Editor/EditorExtension.cs
+++ UnityFramework/Assets/Frame/Scripts/Editor/EditorExtension.cs
@@ -1,6 +1,7 @@
 using Aspose.Cells;
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEngine;
 
@@ -70,7 +71,12 @@ namespace WKC
         private static void ToCSV()
         {
             string excelPath = Application.dataPath + "/ExcelConfig";
-            string csvPath = Application.dataPath + "/CSVConfig/";
+            string csvPath = Application.streamingAssetsPath + "/CSVConfig/";
+
+            if (!Directory.Exists(csvPath))
+            {
+                Directory.CreateDirectory(csvPath);
+            }
 
             string[] files = Directory.GetFiles(excelPath);
             foreach (string file in files)
@@ -88,7 +94,7 @@ namespace WKC
                     StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
                     for (int i = 0; i < temp.Length - 1; i++)
                     {
-                        sw.Write(temp[i]+"\n");
+                        sw.Write(temp[i] + "\n");
                     }
                     sw.Close();
                     fs.Close();
@@ -100,7 +106,7 @@ namespace WKC
         [MenuItem("Tools/Excel转换/转换Json")]
         private static void ToJson()
         {
-            string csvPath = Application.dataPath + "/CSVConfig/ActorConfig.csv";
+            string csvPath = Application.streamingAssetsPath + "/CSVConfig/TestConfig.csv";
             string content = File.ReadAllText(csvPath);
             string[] allContent = content.Split('\n');
             string jsonPath = Application.dataPath + "/Resources/Configs";
@@ -109,62 +115,192 @@ namespace WKC
                 Directory.CreateDirectory(jsonPath);
             }
 
-            string fileName = jsonPath + "/ActorConfig.json";
+            string fileName = jsonPath + "/TestConfig.json";
 
             FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
 
             string head = allContent[1];
             string[] heads = head.Split(',');
-
             string typeLine = allContent[2];
             string[] types = typeLine.Split(',');
-            sw.WriteLine("[\n");
-            for (int i = 3; i < allContent.Length; i++)
+            sw.WriteLine("[");
+
+            for (int i = 3; i < allContent.Length - 1; i++)
             {
-                sw.WriteLine("\t{\n");
                 string line = allContent[i];
                 string[] txt = line.Split(',');
+                if (string.IsNullOrWhiteSpace(txt[0]) || string.IsNullOrEmpty(txt[0]))
+                {
+                    continue;
+                }
+                if (i > 3)
+                {
+                    sw.WriteLine("\t},");
+                }
+                sw.WriteLine("\t{");
 
                 for (int j = 0; j < txt.Length; j++)
                 {
-                    if (j < types.Length)
+                    if (types[j].Trim() == "int" || types[j].Trim() == "float")
                     {
-                        if (types[j] == "int" || types[j] == "float")
+                        if (j == txt.Length - 1)
                         {
-                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":" + txt[j]);
+                            sw.WriteLine("\t\t" + "\"" + heads[j].Trim() + "\"" + ":" + txt[j].Trim());
                         }
-                        else if (types[j] == "string")
+                        else
                         {
-                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":" + "\"" + txt[j] + "\"");
-                        }
-                        else if (types[j] == "List<int>")
-                        {
-                            //string[] list = txt[j].Split(';');
-                            //sw.WriteLine("\t\t[");
-                            //for (int k = 0; k < list.Length; k++)
-                            //{
-
-                            //}
-                            //sw.WriteLine("\t\t]");
-                        }
-                        else if (types[j]=="Dic<,>")
-                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":" + txt[j] + ",");
                         }
                     }
+                    else if (types[j].Trim() == "string")
+                    {
+                        if (j == txt.Length - 1)
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j].Trim() + "\"" + ":" + "\"" + txt[j].Trim() + "\"");
+                        }
+                        else
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":" + "\"" + txt[j] + "\"" + ",");
+                        }
+                    }
+                    else if (types[j].Trim() == "List<int>" || types[j].Trim() == "List<float>")
+                    {
+                        if (j == txt.Length - 1)
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j].Trim() + "\"" + ":");
+                        }
+                        else
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":");
+                        }
+                        string[] list = txt[j].Split(';');
+                        sw.WriteLine("\t\t[");
+                        for (int k = 0; k < list.Length; k++)
+                        {
+                            if (k == list.Length - 1)
+                            {
+                                sw.WriteLine("\t\t\t" + list[k].Trim());
+                            }
+                            else
+                            {
+                                sw.WriteLine("\t\t\t" + list[k].Trim() + ",");
+                            }
+                        }
+                        if (j == txt.Length - 1)
+                        {
+                            sw.WriteLine("\t\t]");
+                        }
+                        else
+                        {
+                            sw.WriteLine("\t\t],");
+                        }
+                    }
+                    else if (types[j].Trim() == "List<string>")
+                    {
+                        if (j == txt.Length - 1)
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j].Trim() + "\"" + ":");
+                        }
+                        else
+                        {
+                            sw.WriteLine("\t\t" + "\"" + heads[j] + "\"" + ":");
+                        }
+                        string[] list = txt[j].Split(';');
+                        sw.WriteLine("\t\t[");
+                        for (int k = 0; k < list.Length; k++)
+                        {
+                            if (k == list.Length - 1)
+                            {
+                                sw.WriteLine("\t\t\t"+ "\""+ list[k].Trim()+"\"");
+                            }
+                            else
+                            {
+                                sw.WriteLine("\t\t\t" + "\"" + list[k].Trim() + "\"" + ",");
+                            }
+                        }
+                        if (j == txt.Length - 1)
+                        {
+                            sw.WriteLine("\t\t]");
+                        }
+                        else
+                        {
+                            sw.WriteLine("\t\t],");
+                        }
+                    }
+                    else if (types[j].Trim() == "Dic<int,int>")
+                    {
+                        
+                    }
                 }
-                if (i == allContent.Length - 1)
+                if (string.IsNullOrEmpty(allContent[i + 1].Trim().Split(',')[0]))
                 {
                     sw.WriteLine("\t}");
-                }
-                else
-                {
-                    sw.WriteLine("\t},");
                 }
             }
             sw.Write("]");
             sw.Close();
             fs.Close();
+            AssetDatabase.Refresh();
+        }
+
+        
+        [MenuItem("Tools/Excel转换/转换C#代码")]
+        private static void ToCSharp()
+        {
+            string csvPath = Application.streamingAssetsPath + "/CSVConfig";
+
+            string[] files = Directory.GetFiles(csvPath);
+            foreach (string file in files)
+            {
+                string suffix = file.Substring(file.Length - 4);
+                if (suffix != "meta")
+                {
+                    string csvName = file.Substring(csvPath.Length + 1);
+                    string className = csvName.Substring(0, csvName.Length - 4);
+                    string path = Application.dataPath + "/Scripts/CSVConfigs";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    string fileName = path + "/" + className + ".cs";
+
+                    string content = File.ReadAllText(file);
+                    string[] temp = content.Split('\n');
+                    
+                    FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+                    sw.WriteLine("using System.Collections.Generic;");
+                    sw.WriteLine("public class "+className);
+                    sw.WriteLine("{");
+
+                    string[] annotation = temp[0].Split(',');
+
+                    string[] fields = temp[1].Split(',');
+                    string[] fieldTyps = temp[2].Split(',');
+                    for (int i = 0; i < fieldTyps.Length; i++)
+                    {
+                        sw.WriteLine("\t/// <summary>");
+                        sw.WriteLine("\t/// " + annotation[i].Trim());
+                        sw.WriteLine("\t/// </summary>");
+                        sw.WriteLine("\tpublic " + fieldTyps[i].Trim().Replace(';', ',') + " " + fields[i].Trim() + " { get; set; }");
+                        sw.WriteLine();
+                    }
+
+
+                    sw.WriteLine("\tpublic static List<" + className + "> GetList()");
+                    sw.WriteLine("\t{");
+                    sw.WriteLine("\t\tList<" + className + "> list = new List<" + className + ">();");
+                    sw.WriteLine("\t\treturn list;");
+                    sw.WriteLine("\t}");
+
+
+                    sw.Write("}");
+                    sw.Close();
+                    fs.Close();
+                }
+            }
             AssetDatabase.Refresh();
         }
         #endregion
